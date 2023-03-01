4840cbe230f8c72e16c45c4e9343ea48c05512ec
Micah Asowata <andrewgary963@gmail.com>
2023-03-01 15:08:18 +0100

    I added some chi middlewares

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -4,19 +4,21 @@ import (
 	"net/http"
 
 	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 )
 
 func (a *application) routes() http.Handler {
-	mux := chi.NewRouter()
-	//TODO: Setup proper routing system
-	//TODO: Setup all the routes
-	mux.Get("/", a.Home)
-	mux.Get("/todo/new", a.NewTodo)
-	mux.Post("/todo/create", a.CreateTodo)
-	mux.Get("/todo/view/{id}", a.GetTodo)
-	mux.Get("/todo", a.Index)
-	mux.Get("/todo/edit/{id}", a.EditTodo)
-	mux.Post("/todo/update/{id}", a.UpdateTodo)
-	mux.Get("/todo/delete/{id}", a.DeleteTodo)
-	return mux
+	router := chi.NewRouter()
+	router.Use(middleware.RealIP)
+	router.Use(middleware.Logger)
+	router.Use(middleware.CleanPath)
+	router.Get("/", a.Home)
+	router.Get("/todo/new", a.NewTodo)
+	router.Post("/todo/create", a.CreateTodo)
+	router.Get("/todo/view/{id}", a.GetTodo)
+	router.Get("/todo", a.Index)
+	router.Get("/todo/edit/{id}", a.EditTodo)
+	router.Post("/todo/update/{id}", a.UpdateTodo)
+	router.Get("/todo/delete/{id}", a.DeleteTodo)
+	return router
 }
